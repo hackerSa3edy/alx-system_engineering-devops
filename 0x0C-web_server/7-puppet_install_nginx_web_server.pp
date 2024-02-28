2cf9b7281e3e8d8c2ce53e11fc39f3150631e9ae
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-02-28 07:38:06 +0200

    [TASK07] Install and configure nginx server using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# Install and configure an Nginx server.
+# perform a 301 redirect when querying /redirect_me.
+
+package { 'nginx':
+  ensure   => present,
+  provider => apt,
+}
+
+file { 'nginx_index.html':
+  ensure  => file,
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+
+$redir_conf = '\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}'
+$cmd = "sed -i \"53i\\ ${redir_conf}\" /etc/nginx/sites-enabled/default &> /dev/null"
+
+exec { 'redirect_path':
+  command => $cmd,
+  path    => '/usr/bin',
+  require => Package['nginx'],
+}
